9e1c6586f6fe60fa911da7761ceb4c02192519c3
christopher818 <christo.8.16@gmail.com>
2019-05-31 12:35:08 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
 
+parse (buf,args)
+char *buf;
+char **args;
+{
+	while (*buf != NULL){
+		/* frajas en blanco
+		* usa nulo para que el argumento anterior se termine
+		*/	
 
+		while ((*buf == ''))  ||  ((*buf == '\t' ))	
+			*buf++ = NULL;
+
+		/* salva los argumentos */
+		*args++ = buf;
+
+		/* omite los argumentos */ 
+		while ((*buf != NULL) && (*buf != '' ) && (*buf != '\t'))
+			buf++;
+	}
+
+	*args = NULL;
+}
+
+/* execute
+genera un proceso hijo y ejecuta los programas
+*/
+
+execute(args)
+char **args;
+{
+	int pid, status;
+
+	/* 
+	obtiene el proceso hijo
+	*/
+
+	if ((pid = fork()) < 0){
+		perror(“fork”);
+		exit(1);
+	}
+
+	/* el hijo ejecuta el codigo dentro del if
+	*/
+
+	if (pid == 0){
+		execvp(*args,args);
+		perror(*args);
+		exit(1);
+	}
+
+	/*
+	el padre ejecuta la espera
+	*/ 
+	while (wait(&status) != pid)
+		/* vacio*/;
+}
 
 
 
