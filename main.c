92d104631beb85517f81efc3aa93dbef4525235d
María Fernanda Magallanes <35668326+MaferMazu@users.noreply.github.com>
2019-05-31 12:34:13 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+
+
+main()
+{
+  char buf[1024];
+  char *args[64];
+
+  for (;;) {
+  /*Lectura de comandos*/
+  printf(“>”);
+  if (gets(buf) == NULL){
+    printf(“/n”)}
+  }
+}
